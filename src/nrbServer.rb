2efe286a93e06be289e2e28262e698d74207040c
sovereign <astartes@live.ru>
2020-05-03 16:27:50 +1000

    update

--- src/nrbServer.rb
+++ src/nrbServer.rb
@@ -3,12 +3,11 @@ require './Server.rb'
 Task.max = 20
 
 serv = Server.new
+serv.set_work_dir('./data')
 serv.read_conf
 serv.read_keys
 Task.queue {
   serv.start(5, 3)
 }
-
-puts serv.ls('2f1j48ijf214891j4712fjj982f1j48')
 
 Task.wait
